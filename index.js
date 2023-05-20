95ea7acdf7d6c9e93898932d91019d6f8a8398cf
shynu S.J <shynujose11@gmail.com>
2023-05-20 21:25:49 +0530

    v1.0.2

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Silverstorm started on port ${process.env.PORT}`);
 });
